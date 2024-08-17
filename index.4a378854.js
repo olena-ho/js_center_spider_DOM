070cd73136b07453a68f0ed829b3b5fed7860c4d
Olena Horovenko <elena.gorovenko@gmail.com>
2024-08-17 10:58:52 +0200

    make build

---
+++ index.4a378854.js
@@ -0,0 +1,2 @@
+Object.assign(document.querySelector(".spider").style,{left:"50%",top:"50%",transform:"translate(-50%, -50%)"});
+//# sourceMappingURL=index.4a378854.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spiderElement = document.querySelector(".spider");
+Object.assign(spiderElement.style, {
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)"
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
